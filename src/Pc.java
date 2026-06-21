32670b0deb3a15e974b7c440fee1d9b922218fab
MENOB <senjumenob@gmail.com>
2026-06-21 20:24:32 -0300

    add tamanho das memorias

--- src/Pc.java
+++ src/Pc.java
@@ -1,3 +1,14 @@
 public class Pc {
     
+    public Pc(){
+        //construtor vazio
+    }
+    //de acordo o diagrama é x
+    public static final int TAMANHO_MEMORIA_FISICA = 16; //pode ser alterada e n vai ter que mexer em mais nada p fazer essa alteração
+    
+    // de acordo o diagrama q a gente montou onde era 2x
+    public static final int TAMANHO_DISCO = TAMANHO_MEMORIA_FISICA * 2; //pode ser alterada e n vai ter que mexer em mais nada p fazer essa alteração
+    
+    // de acordo o diagrama q a gente montou onde era 2x
+    public static final int TAMANHO_MEMORIA_VIRTUAL = TAMANHO_MEMORIA_FISICA * 2; //pode ser alterada e n vai ter que mexer em mais nada p fazer essa alteração
 }
