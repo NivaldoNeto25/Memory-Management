f38d43f54d095cb08f758cfe89b5881c68403e4e
NivaldoNeto25 <nivaldonetocontato@gmail.com>
2026-06-20 22:22:04 -0300

    Atualizaçao no construtor da classe

--- src/util/Instrucao.java
+++ src/util/Instrucao.java
@@ -2,18 +2,30 @@ package util;
 
 public class Instrucao {
 
-    // Classe POJO (Plain Old Java Object) cria um objeto focado apenas em acomodar dados
+    // Classe POJO (Plain Old Java Object): cria um objeto focado apenas em acomodar dados.
+    // Não possui lógica de negócio, cálculos ou comportamento complexo.
 
     private int enderecoVirtual;
     private char operacao;
-    private Integer valor; // Tem q usar o Integer ao invés do int, para o caso de em uma operaçao de leitura (ex: 1-R) o "valor" nao receber o valor = 0. Se fosse int e fosse uma operaçao de leitura, o java forçaria o "valor" para 0.
+    
+    private Integer valor;      // Tem q usar o Integer ao invés do int, para o caso de em uma operaçao de leitura (ex: 1-R) 
+                                // o "valor" nao recebe valor = 0. Se fosse int, o Java forçaria o "valor" para 0, 
+                                // Isso para o caso de operações de leitura
 
+    // Constutor para operações de escrita ('W')
     public Instrucao(int enderecoVirtual, char operacao, Integer valor) {
         this.enderecoVirtual = enderecoVirtual;
         this.operacao = operacao;
         this.valor = valor;
     }
 
+    // Construtor para operações de leitura ('R')
+    public Instrucao(int enderecoVirtual, char operacao) {
+        this.enderecoVirtual = enderecoVirtual;
+        this.operacao = operacao;
+    }
+
+    // Não possui Setters para não modificar os dados
     public int getEnderecoVirtual() { 
         return enderecoVirtual; 
     }
