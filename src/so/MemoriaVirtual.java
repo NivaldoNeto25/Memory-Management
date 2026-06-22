5423024b36a2f85fcc461c45dfb19246b2406f5b
MENOB <senjumenob@gmail.com>
2026-06-21 22:29:20 -0300

    criando memoria virtual

--- src/so/MemoriaVirtual.java
+++ src/so/MemoriaVirtual.java
@@ -9,4 +9,15 @@ public class MemoriaVirtual {
         this.espacoVirtual = new Pagina[tamanho];
     }
     
+    public void registrarPagina(int enderecoVirtual, Pagina pagina){ //vai ser usado na inicializacao do sistema p criar as paginas logicas
+        this.espacoVirtual[enderecoVirtual] = pagina;
+    }
+
+    public Pagina getPaginaLogica(int enderecoVirtual){ //vai retornar a pagina baseado no endereco logico solicitado pela instrucao
+        return this.espacoVirtual[enderecoVirtual];
+    }
+
+    public int getTamanhoTotal(){ //aq e para a mmu saber o limite dos enderecos
+        return this.espacoVirtual.length;
+    }
 }
