0884a0401328b4eaa3f76b3eb1762cf3fb8686db
NivaldoNeto25 <nivaldonetocontato@gmail.com>
2026-06-25 18:44:04 -0300

    Adicionando o método do printer

--- src/util/Printer.java
+++ src/util/Printer.java
@@ -1,5 +1,19 @@
 package util;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Printer {
-    
+
+    // padrão visual de tempo: Hora:Minuto:Segundo.Milissegundo
+    private static final DateTimeFormatter formatadorTempo = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+
+    //Se duas threads chamarem o print ao mesmo tempo, uma espera a outra terminar
+    // O 'static' permite chamar Printer.log sem instanciar a classe
+    public static synchronized void log(String mensagem) {
+        String horaAtual = LocalTime.now().format(formatadorTempo);
+        
+        // Vai mostrar no terminal a hora e a mensagem
+        System.out.println("[" + horaAtual + "] : " + mensagem);
+    }
 }
