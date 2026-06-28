8d41c187c77d0d0ea02a19f82b6a6ced5ec473f3
NivaldoNeto25 <nivaldonetocontato@gmail.com>
2026-06-28 03:32:51 -0300

    Criando a classe das threads

--- src/processos/ProcessoThread.java
+++ src/processos/ProcessoThread.java
@@ -1,5 +1,43 @@
 package processos;
 
-public class ProcessoThread {
+import hardware.MMU;
+import util.Instrucao;
+import util.Printer;
+import java.util.List;
+
+// O 'extends Thread' permite que o Java rode essa classe em paralelo
+public class ProcessoThread extends Thread {
     
+    private int idProcesso;
+    private List<Instrucao> filaInstrucoes;
+    private MMU mmu;
+
+    // Construtor recebe a identidade, a fatia de instruções exclusiva dela e o "cérebro" (MMU)
+    public ProcessoThread(int idProcesso, List<Instrucao> filaInstrucoes, MMU mmu) {
+        this.idProcesso = idProcesso;
+        this.filaInstrucoes = filaInstrucoes;
+        this.mmu = mmu;
+    }
+
+    // O método run() dita o que acontece no momento em que o PC dá o "thread.start()"
+    @Override
+    public void run() {
+        Printer.log("Thread " + idProcesso, "Iniciada com " + filaInstrucoes.size() + " instruções.");
+        
+        // Varre cada instrução do seu lote e manda para a MMU processar
+        for (Instrucao inst : filaInstrucoes) {
+            
+            mmu.processarAcesso(inst, idProcesso);
+            
+            // Pausa de 100 milissegundos para simular o tempo de processamento da CPU
+            // Isso também serve para você conseguir ler o log no console com calma
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Printer.log("Thread " + idProcesso, "Foi interrompida.");
+            }
+        }
+        
+        Printer.log("Thread " + idProcesso, "Finalizou todas as suas instrucoes com sucesso!");
+    }
 }
