1841366d78553787085b67359e9868f5f7bc5f0b
NivaldoNeto25 <nivaldonetocontato@gmail.com>
2026-06-28 00:48:04 -0300

    Alterando o método do printer para adicionar o emissor da mensagem

--- src/util/Printer.java
+++ src/util/Printer.java
@@ -10,10 +10,10 @@ public class Printer {
 
     //Se duas threads chamarem o print ao mesmo tempo, uma espera a outra terminar
     // O 'static' permite chamar Printer.log sem instanciar a classe
-    public static synchronized void log(String mensagem) {
+    public static synchronized void log(String emissor, String mensagem) {
         String horaAtual = LocalTime.now().format(formatadorTempo);
         
         // Vai mostrar no terminal a hora e a mensagem
-        System.out.println("[" + horaAtual + "] : " + mensagem);
+        System.out.println("[" + horaAtual + "] [" + emissor + "]: " + mensagem);
     }
 }
